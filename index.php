3657333c352ea4eebe8d4f497ebee8b8d8f488f4
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-09-04 19:25:55 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home - GK Learn Study</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to GK Learn Study</h1>
+    <p>Yahan aapka content hoga.</p>
+
+    <!-- Footer include -->
+    <?php include 'footer.php'; ?>
+</body>
+</html>
